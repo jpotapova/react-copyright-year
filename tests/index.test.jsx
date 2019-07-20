4ee3f22676dfc6956ad77afd1017230a4a9c2a95
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2019-07-21 00:41:19 +0300

    Setup enzyme

--- tests/index.test.jsx
+++ tests/index.test.jsx
@@ -1,5 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import CopyrightYear from '../src';
+
 describe('Example suite', () => {
   it('example test', () => {
-
+    shallow(<CopyrightYear />);
   });
 });
