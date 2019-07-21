3776112fbe4b3be66e4fe4396a84642429b10549
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2019-07-21 17:17:11 +0300

    Add current year tests

--- tests/index.test.jsx
+++ tests/index.test.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import MockDate from 'mockdate';
 
 import CopyrightYear from '../src';
 
 describe('CopyrightYear', () => {
   it('renders without crashing', () => {
     shallow(<CopyrightYear />);
+  });
+
+  describe('without customisation', () => {
+    it('renders copyright symbol with a year', () => {
+      MockDate.set('2019');
+      const wrapper = shallow(<CopyrightYear />);
+
+      expect(wrapper.text()).toBe('© 2019');
+      MockDate.reset();
+    });
+
+    it('renders copyright symbol with a year', () => {
+      MockDate.set('2022');
+      const wrapper = shallow(<CopyrightYear />);
+
+      expect(wrapper.text()).toBe('© 2022');
+      MockDate.reset();
+    });
   });
 });
