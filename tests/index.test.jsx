a23b85c9cf96615844962d7278a6b531f36906db
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2019-07-22 00:25:19 +0300

    Add tests for prefix & suffix

--- tests/index.test.jsx
+++ tests/index.test.jsx
@@ -10,7 +10,7 @@ describe('CopyrightYear', () => {
   });
 
   describe('without customisation', () => {
-    it('renders copyright symbol with a year', () => {
+    it('renders copyright symbol with a year 2019', () => {
       MockDate.set('2019');
       const wrapper = shallow(<CopyrightYear />);
 
@@ -18,12 +18,40 @@ describe('CopyrightYear', () => {
       MockDate.reset();
     });
 
-    it('renders copyright symbol with a year', () => {
+    it('renders copyright symbol with a year 2022', () => {
       MockDate.set('2022');
       const wrapper = shallow(<CopyrightYear />);
 
       expect(wrapper.text()).toBe('© 2022');
       MockDate.reset();
     });
+  });
+
+  describe('with prefix', () => {
+    it('renders copyright symbol with a year 2016 & prefix', () => {
+      MockDate.set('2016');
+      const wrapper = shallow(<CopyrightYear prefix="Copyright " />);
+
+      expect(wrapper.text()).toBe('Copyright © 2016');
+      MockDate.reset();
+    });
+
+    it('and suffix renders copyright symbol with a year 2030 & prefix & suffix', () => {
+      MockDate.set('2030');
+      const wrapper = shallow(
+        <CopyrightYear prefix="Copyright " suffix=" example-site.com" />
+      );
+
+      expect(wrapper.text()).toBe('Copyright © 2030 example-site.com');
+      MockDate.reset();
+    });
+  });
+
+  it('with suffix renders copyright symbol with a year 2024 & suffix', () => {
+    MockDate.set('2024');
+    const wrapper = shallow(<CopyrightYear suffix=" example-site.com" />);
+
+    expect(wrapper.text()).toBe('© 2024 example-site.com');
+    MockDate.reset();
   });
 });
