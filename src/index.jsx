898c8e281bd339ad558a3dedf7b94add3ce472fc
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2019-07-22 00:25:42 +0300

    Implement prefix & suffix

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function CopyrightYear() {
-  return <div>{`© ${new Date().getFullYear()}`}</div>;
+function CopyrightYear(props) {
+  const { prefix, suffix } = props;
+  return <div>{`${prefix}© ${new Date().getFullYear()}${suffix}`}</div>;
 }
+
+CopyrightYear.defaultProps = {
+  prefix: '',
+  suffix: ''
+};
+
+CopyrightYear.propTypes = {
+  prefix: PropTypes.string,
+  suffix: PropTypes.string
+};
 
 export default CopyrightYear;
